577b9a67a8d83fb4008d5983a4a9ae6dac75c7be
JuhaszZoltan <juhaszzoltanmate@yahoo.com>
2020-11-11 11:01:30 +0100

    add expl

--- CA201111/Kutya.cs
+++ CA201111/Kutya.cs
@@ -8,10 +8,14 @@ namespace CA201111
 {
     class Kutya
     {
+        //auto-propety
         public string Nev { private set; get; }
+        //field ("mező" in HUN)
         private DateTime _szul;
+        //property ("jellemző" in HUN)
         public DateTime Szul
         {
+            //public prop-on belül private set
             private set
             {
                 if (value > DateTime.Now) 
@@ -21,6 +25,7 @@ namespace CA201111
             get { return _szul; }
         }      
 
+        //only-get property ("csak lovasható jellemző" in HUN ::: NEM ÖSSZEKEVERENDŐ a readonly mezővel)
         public int Kor
         {
             get
@@ -29,19 +34,21 @@ namespace CA201111
             }
         }
 
+        //mező alapértelmezett értékkel
         private int _labak = 4;
         public int Labak
         {
             set
             {
-                if (value > 4 || value < 0) throw new Exception("nem lehet ennyi lába egy kutyának");
+                if (value > 4 || value < 0) 
+                    //rise an exception ("kivételt dobni" in HUN aka. "hibaüzenet")
+                    throw new Exception("nem lehet ennyi lába egy kutyának");
                 _labak = value;
             }
             get { return _labak; }
         }
 
-
-        //metódusok (method)
+        //method ("tagfüggvény" in HUN ::: fura, de akkor is tagfüggvénynek hívjuk, ha valójában eljárás... magyar nyelv csodái)
         public void Ugat()
         {
             Console.WriteLine($"{Nev} mondja: vau-vau!");
@@ -50,15 +57,19 @@ namespace CA201111
         public bool Szereti(string dolog)
         {
             if (dolog == "csont") return true;
+            if (dolog == "zsömle") return true;
             if (dolog == "kelbimbó") return false;
             throw new Exception($"a {dolog}-ról nem ismert, hogy szereti-e ez a kutya");
         }
 
+        //constructor ("konstruktor" in HUN ::: na EZT sikerült megfelelően "magyarosítani :'D)
         public Kutya(string nev, DateTime szul)
         {
             Nev = nev;
             Szul = szul;
         }
+        //constructor-overload + constructor-inheritence
+        //(overloading-ra nem tudom van-e magyar szó, az con-inh-et "származtatott konstruktor"nak szokták hívni)
         public Kutya(string nev, DateTime szul, int labak) : this(nev, szul)
         {
             Labak = labak;
